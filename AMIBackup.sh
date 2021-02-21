a8b653cac4aca5a2c6238c3048c3c021c5210bee
Ashish Kamble <kambleashish004@gmail.com>
2021-02-21 20:22:29 +0530

    Update AMIBackup.sh
    Made some changes.

--- AMIBackup.sh
+++ AMIBackup.sh
@@ -1,7 +1,5 @@
 today=$(date +"%Y-%m-%d")
 instanceid=`cat serverlistinstance.txt | awk -F " " '{print $1}'`
-ticket=`cat ticket.txt`
-echo $ticket
 
 for instance in $instanceid; do
 echo $instance
@@ -10,16 +8,11 @@ echo $InstanceName >> scriptdata.txt
 echo $InstanceName
 Deployment=$(aws ec2 describe-instances --instance-id $instance --query 'Reservations[*].Instances[*].[Tags[?Key==`Deployment`].Value]' --output text)
 echo $Deployment
-#Project=$(aws ec2 describe-instances --instance-id $instance --query 'Reservations[*].Instances[*].[Tags[?Key==`Project`].Value]' --output text)
-#echo $Project
 echo $InstanceName >> scriptdata.txt
 
 
-
-AMID=$(aws ec2 create-image --instance-id $instance --name $ticket-$InstanceName-$today-GOLDEN --description $ticket-$InstanceName-$today-GOLDEN --no-reboot --output text)
+AMID=$(aws ec2 create-image --instance-id $instance --name $InstanceName-$today --description $ticket-$InstanceName-$today-GOLDEN --no-reboot --output text)
 echo $AMID >> scriptdata.txt
 echo $AMID
-aws ec2 create-tags --resources $AMID --tags Key=Name,Value=$InstanceName Key=Project,Value=LASTSYSTEMBACKUP Key=Deployment,Value=$Deployment Key=InstanceId,Value=$instance Key=Validity,Value=$NEXT_DATE
-echo "Tags Added " >> scriptdata.txt
 
 done
